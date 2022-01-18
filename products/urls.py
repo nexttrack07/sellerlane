68943b8b63efba1f82a7123524fbc930074df905
Faadhil Hakim <faadhil@nexttrack.biz>
2022-01-17 22:31:36 -0600

    update styles, add nav

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='book_list'),
+    path('', ProductListView.as_view(), name='product_list'),
     path('<uuid:pk>', ProductDetailView.as_view(), name='product_detail')
 ]
